d370d978302945db5f6d5296e4f5b48e801be495
Kyle Humphrey <khumph2@gmail.com>
2023-11-11 10:36:48 -0800

    Inbox entries

--- config.el
+++ config.el
@@ -64,7 +64,7 @@
   (visual-line-mode 1)
   (add-to-list 'org-modules 'org-checklist)
   (add-to-list 'org-capture-templates
-               '("i" "Inbox" plain (file "inbox.org") "%?"))
+               '("i" "Inbox" entry (file "inbox.org") "* %?"))
   (setq org-directory "~/Desktop/org"
         org-agenda-files (directory-files-recursively "~/Desktop" "\\.org$")
         org-archive-location "archive.org::* %s"
